4f1cf7973b362f58fb15ff61df8c1da530bab934
Christophe Schevers <christophe.schevers@gmail.com>
2019-07-02 16:22:17 +0200

    update

--- exercise-2/html.php
+++ exercise-2/html.php
@@ -1,9 +1,51 @@
 <?php
 class Html{
+    public $begin = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge">';
+    public $mid = '</head><body>';
+    public $end = '</body></html>';
+
     public function __construct()
     {
         //
     }
 
-    
+    public function begin()
+    {
+        return $this->begin;
+    }
+
+    public function meta($name,$content)
+    {
+        return '<meta name="'.$name.'" content="'.$content.'">';
+    }
+
+    public function title($title)
+    {
+        return '<title>'.$title.'</title>';
+    }
+
+    public function mid()
+    {
+        return $this->mid;
+    }
+
+    public function img($url)
+    {
+        return '<img src="'.$url.'">';
+    }
+
+    public function link($url,$text)
+    {
+        return '<a href="'.$url.'">'.$text.'</a>';
+    }
+
+    public function script($url)
+    {
+        return '<script src="'.$url.'"></script>';
+    }
+
+    public function end()
+    {
+        return $this->end;
+    }
 }
--- exercise-2/index.php
+++ exercise-2/index.php
@@ -1,2 +1,12 @@
 <?php
 include 'html.php';
+
+$page = new Html();
+echo $page->begin();
+echo $page->meta('author','Christophe Schevers');
+echo $page->title('Html builder class');
+echo $page->mid();
+echo $page->img('https://img.static-rmg.be/a/view/q75/w720/h/2170223/lat-png.png');
+echo $page->link('https://lmgtfy.com/?q=google','Google');
+echo $page->script('test.js');
+echo $page->end();
